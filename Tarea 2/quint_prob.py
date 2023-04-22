5923fa43cbf2effd39e0088ad3b1d3bd43dbd505
arianneclv <arianne102001@gmail.com>
2023-04-22 17:50:27 -0600

    Tarea 2

---
+++ Tarea 2/quint_prob.py
@@ -0,0 +1,13 @@
+est = { 
+    "name" : "Mike",
+    "physics" : 70,
+    "history" : 80,
+    "math" : 90,
+}
+
+promedio = (est.get("physics") + est.get("history") + est.get("math"))/(len(est)-1)
+
+print(promedio)
+
+#Profe para poder entregar algo de este ejercicio lo hice así :c , no me leía las diferentes llaves 
+#si lo introducía con el diccionario original, no sé si podría subir la solución, gracias.
