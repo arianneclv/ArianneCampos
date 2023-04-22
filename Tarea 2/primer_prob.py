f6f1411628018815d48376c7b30de481319f867c
arianneclv <arianne102001@gmail.com>
2023-04-22 12:20:01 -0600

    Problema 1 Factorial

--- Tarea 2/primer_prob.py
+++ Tarea 2/primer_prob.py
@@ -2,7 +2,8 @@ numero=int(input("Ingrese el numero: "))
 if numero < 0:
     print("Ingrese valores válidos.")
 else:
-    Factorial = 1
+    factorial = 1
     for i in range(1,numero+1):
-        Factorial = Factorial*i
-    print("El factorial es " + str(Factorial))
+        factorial = factorial*i
+    print("El factorial es " + str(factorial))
+    
